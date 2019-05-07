208eadf0c43841441c5c7066bcaba551cb578dee
biechuyangwang <13799297+biechuyangwang@users.noreply.github.com>
2019-05-07 19:29:20 +0800

    updata p2

--- my_algorithms/cpp/Add_Two_Numbers/Add_Two_Numbers.cpp
+++ my_algorithms/cpp/Add_Two_Numbers/Add_Two_Numbers.cpp
@@ -1,3 +1,22 @@
+// Source : https://oj.leetcode.com/problems/add-two-numbers/
+// Author : Jun Lee
+// Date   : 2019-05-06
+
+/********************************************************************************** 
+* 
+* You are given two non-empty linked lists representing two non-negative integers. 
+* The digits are stored in reverse order and each of their nodes contain a single digit. 
+* Add the two numbers and return it as a linked list.
+* 
+* You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+*  
+* Example:
+*  
+* Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+* Output: 7 -> 0 -> 8
+* Explanation: 342 + 465 = 807.
+*  
+**********************************************************************************/
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -6,7 +25,7 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution { 
+class Solution {  
 public: 
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) { 
         ListNode *ans = NULL, **t = &ans;  //t用来控制指针的指向。 
